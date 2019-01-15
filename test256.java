400037da8027e4e3ce7566ce3f11ba8c433cc630
snahil28 <ss11381@nyu.edu>
2019-01-14 20:20:25 -0500

    Create test256.java
    Password cracking for formspring- SHA256

---
+++ test256.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class test256 {
+	public static void main (String[] args) throws FileNotFoundException {
+		BufferedReader reader = null;
+		File outputFile = new File("C:/Users/Snahil/Downloads/Password Dump/formspring/formspring/output-256-11.rtf");
+        FileOutputStream is1 = new FileOutputStream(outputFile);
+        OutputStreamWriter osw1 = new OutputStreamWriter(is1);    
+        Writer w1 = new BufferedWriter(osw1);
+        String line;
+	    try{
+	        reader = new BufferedReader(new FileReader("C:/Users/Snahil/Downloads/Password Dump/formspring/formspring/linkedin_passwords_dump11.txt"));
+	        File statText = new File("C:/Users/Snahil/Downloads/Password Dump/formspring/formspring/comp1_encrypted11.rtf");
+	        File file2 = new File("C:/Users/Snahil/Downloads/Password Dump/formspring/formspring/formspring.txt");
+	        Scanner scan = null;
+	        Scanner scan1= null;
+	        scan = new Scanner(file2);
+	        FileOutputStream is = new FileOutputStream(statText);
+	        OutputStreamWriter osw = new OutputStreamWriter(is);    
+	        Writer w = new BufferedWriter(osw);
+	        while ((line = reader.readLine()) != null) {
+	        	String base = "00" + line;
+	        	MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		        byte[] hash = digest.digest(base.getBytes("UTF-8"));
+		        StringBuffer hexString = new StringBuffer();
+
+		        for (int i = 0; i < hash.length; i++) {
+		            String hex = Integer.toHexString(0xff & hash[i]);
+		            if(hex.length() == 1) hexString.append('0');
+		            hexString.append(hex);
+		        }
+		        System.out.println(hexString.toString());
+		        String[] words = line.split(":");
+		        String hexCode = "00000" + hexString.substring(5);
+		        w.write(hexCode+","+words[0]);
+		        w.write(System.getProperty( "line.separator"));
+   	        }
+	        w.close();
+	        scan1 = new Scanner(statText);
+	       ArrayList<String> dictionary = new ArrayList<String>();
+	        while(scan1.hasNextLine())
+	        {
+	        	String str = null;
+	            str = scan1.nextLine();
+	            dictionary.add(str);
+	        }
+	        int counting = 0;
+	        while(scan.hasNextLine())
+	        {
+	        	String str1 = scan.nextLine();
+	        	System.out.println("str1: "+str1);
+	       		boolean flag = false;
+	        	for (String s: dictionary)
+	        	{
+	        		String[] words = s.split(",");
+	        		System.out.println("s is : "+s);
+	        		if(str1.equals(words[0]))
+	        		{
+	        			counting++;
+	        			w1.write(s);
+             			w1.write(System.getProperty( "line.separator"));
+             			flag = true;
+	        		}
+	        		if (flag == true)
+	    				break;;
+	           	}
+	        	
+	        	System.out.println("counting: "+counting);
+	        }
+	       
+	        w1.close();
+	        scan.close();
+	        scan1.close();
+	    } catch(Exception ex){
+	       throw new RuntimeException(ex);
+	    }
+	}
+}
